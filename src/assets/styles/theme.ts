279a87bc7e2c9a79ef81fc6703de36ef1f719104
alongw <alongw@alongw.cn>
2025-05-05 23:17:56 +0800

    feat: custom theme

---
+++ src/assets/styles/theme.ts
@@ -0,0 +1,11 @@
+import { type ThemeDefinition } from 'vuetify'
+
+export const lightTheme: ThemeDefinition = {
+    dark: false,
+    colors: {
+        background: '#FFFFFF',
+        surface: '#FFFFFF',
+        primary: '#5BCEFA',
+        secondary: '#F5A9B8'
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,22 @@
 import 'vuetify/styles'
 import '@mdi/font/css/materialdesignicons.css'
 
+import { lightTheme } from '@/assets/styles/theme'
+
 import { createApp } from 'vue'
 import { createVuetify } from 'vuetify'
 import App from './App.vue'
 import router from './router'
 const app = createApp(App)
-const vuetify = createVuetify()
+const vuetify = createVuetify({
+    theme: {
+        defaultTheme: 'lightTheme',
+        themes: {
+            lightTheme
+        }
+    }
+})
 
 app.use(router)
 app.use(vuetify)
 app.mount('#app')
-
-export { vuetify }
