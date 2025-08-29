8d1ddc8a893c9aabb7825a1961099ff15e6fa3c0
Mxmilu666 <mxmilu666@gmail.com>
2025-08-30 07:50:56 +0800

    feat: add vite-plugin-sitemap for dynamic routing

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,22 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import vuetify from 'vite-plugin-vuetify'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vuetify(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+    plugins: [
+        vue(),
+        vuetify(),
+        vueDevTools(),
+        Sitemap({
+            hostname: 'https://network.lolinya.net',
+            dynamicRoutes: ['/', '/communities']
+        })
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    }
 })
